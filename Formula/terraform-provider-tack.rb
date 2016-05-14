a5f22bcf69929f4c4b383b21b0324c308fea60d2
Jono Wells <on@oj.io>
2016-05-13 18:17:18 -0700

    Update terraform-provider-tack.rb

--- Formula/terraform-provider-tack.rb
+++ Formula/terraform-provider-tack.rb
@@ -1,16 +1,29 @@
 require "language/go"
 
 class TerraformProviderTack < Formula
+#  head "https://github.com/some/package.git", :branch => "develop"
   desc "Terraform provider to support the Tack project"
-  homepage "https://github.com/kz8s/terraform-provider-tack"
+  homepage "https://github.com/kz8s/terraform-tack"
   url "https://github.com/kz8s/terraform-tack/archive/develop.tar.gz"
+  version "0.0.1"
 
   depends_on "go" => :build
 
   def install
+    ENV["GOBIN"] = bin
     ENV["GOPATH"] = buildpath
-    system "go", "build", "-o", "terraform-provider-tack"
-    bin.install "terraform-provider-tack"
+    ENV["GOHOME"] = buildpath
+
+    path = buildpath/"src/github.com/kz8s/terraform-tack"
+    path.install Dir["*"]
+
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd path do
+        system "go", "get"
+        system "go", "build", "-o", "terraform-provider-tack"
+        bin.install "terraform-provider-tack"
+    end
   end
-  
+
 end
